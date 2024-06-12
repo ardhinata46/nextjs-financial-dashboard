d2393df3c87cd2376cea37e864948dfef1a612a2
ardhinata46 <ardhinata46@gmail.com>
2024-06-12 12:12:55 +0700

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Roboto_Mono } from 'next/font/google';
+import {subset} from "semver";
+
+export const inter = Inter({ subsets: ['latin']});
+export const lusitana = Roboto_Mono({ subsets: ['latin'] });
