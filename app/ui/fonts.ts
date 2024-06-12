154c9c2f614e0f276af5790ff5d42b037c1a437f
ardhinata46 <ardhinata46@gmail.com>
2024-06-12 12:20:19 +0700

    update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,4 @@
 import { Inter, Roboto_Mono } from 'next/font/google';
-import {subset} from "semver";
 
 export const inter = Inter({ subsets: ['latin']});
 export const lusitana = Roboto_Mono({ subsets: ['latin'] });
